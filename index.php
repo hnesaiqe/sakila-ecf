b0e82199bf71bdc34ee55376364b0c867ff1ba6c
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-08-19 11:02:50 +1100

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 include './views/parts/head.php';
 include './views/parts/header.php';
-include './views/parts/footer.php';;
- ?>
+?>
+<section >
+    <div class="col-md-12 text-center mx-auto my-5">
+        <button class="btn btn-outline-info" type="submit">
+            <h1>
+                consulter
+            </h1>
+        </button>
+        <button class="btn btn-outline-secondary" type="submit">
+            <h1>
+                Louer
+            </h1>
+        </button>
+    </div>
+</section>
+<?php
+include './views/parts/footer.php';?>
--- views/parts/header.php
+++ views/parts/header.php
@@ -1,13 +1,13 @@
 <body style="font-family:dank Mono">
   <div class="title text-center">
     <h1>
-      SAKILA
+      SAKILA-DVD location
     </h1>
   </div>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-success">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Ecf</a>
+      <a class="navbar-brand" href="#">Ecf 7</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -18,25 +18,18 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Consulter</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
               aria-expanded="false">
-              Dropdown
+              Location
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Louer</a></li>
+              <li><a class="dropdown-item" href="#">Réception</a></li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
+          </li>          
         </ul>
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
